e29bec8ece1b5c78010a039e3014e38b44047bf8
Karol Majcher <karolmajcher8@gmail.com>
2025-10-20 18:13:25 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+
+
+
+class Config:
+    WEATHER_API_KEY = "7856f1fd6050da689353cca330d34d52"
+    CITY = "Lisbon"
+    EXCEL_FILEPATH = "pogoda.xlsx"
---
+++ main.py
@@ -0,0 +1,17 @@
+import config
+from config import Config
+
+print(Config.WEATHER_API_KEY)
+print(Config.CITY)
+
+# tworzenie repozytorium
+# git init
+
+# sprawedzanie stanu repozytorium
+# git status
+
+# dodanie pliku do repo
+# git add nazwa
+
+# dodanie wszystkiego
+# git add .
